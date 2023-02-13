61067999e94f00c2dc274345a29049479d213667
Igor <po4talen@gmail.com>
2023-02-13 22:11:49 +0200

    Lesson3

---
+++ Les3/Lesson3_switch.js
@@ -0,0 +1,54 @@
+/*Перепишіть конструкцію "switch" в аналогічну з використанням "if"
+switch (browser) {
+  case 'Edge':
+    alert( "You've got the Edge!" );
+    break;
+  case 'Chrome':
+  case 'Firefox':
+  case 'Safari':
+  case 'Opera':
+    alert( 'Ми підтримуємо і ці браузери' );
+    break;
+
+  default:
+    alert( 'Маємо надію, що ця сторінка виглядає добре!' );} */
+let browser = 'Safari';
+if (browser == `Edge`) {
+    alert("You've got the Edge!");
+}
+else if (browser == 'Chrome' || 'Safari' || 'Opera') {
+    alert('Ми підтримуємо і ці браузери');
+}
+else if (browser == 'Opera') {
+    alert('Ми підтримуємо і ці браузери');
+}
+else {
+    alert('Маємо надію, що ця сторінка виглядає добре!');
+}
+
+/*Перепишіть умови "if" в конструкцію "switch"
+let a = +prompt('a?', '');
+if (a == 0) {
+  alert( 0 );
+}
+if (a == 1) {
+  alert( 1 );
+}
+
+if (a == 2 || a == 3) {
+  alert( '2,3' );}*/
+
+let a = +prompt('a?', '');
+switch (a) {
+    case 0:
+        alert(0);
+        break;
+
+    case 1:
+        alert(1);
+        break;
+    case 2:
+    case 3:
+        alert('2,3')
+        break;
+}
---
+++ Les3/Lesson3_while_for.js
@@ -0,0 +1,46 @@
+/* Виведіть парні числа
+Виведіть парні числа від 2 до 10, використовуючи цикл for.*/
+
+for (let i = 2; i <= 10; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+}
+
+/* Замініть цикл "for" на "while"
+for (let i = 0; i < 3; i++) {
+  alert( `число ${i}!` );} */
+
+let i = 0;
+while (i <= 3) {
+    console.log(`Number ${i}!`)
+    i++;
+}
+
+/*Повторяти цикл, доки ввід невірний
+Напишіть цикл, який пропонує prompt ввести число більше за 100. Якщо відвідувач введе інше число – попросити ввести ще раз, і так далі.
+Цикл повинен запитувати число доти, доки відвідувач не введе число, більше за 100, або не скасує ввід/введе порожній рядок.
+Ми припускаємо, що відвідувач вводитиме лише числа. В цьому завданні не обов’язково реалізовувати оброблення не-числового введення. */
+
+let enteredValue = 0;
+
+do {
+    enteredValue = prompt(`Enter the number > 100`,);
+    if (enteredValue == null || enteredValue == "")
+        break;
+}
+while (enteredValue <= 100);
+
+
+/*Вивести прості числа
+Напишіть код, який виводить всі прості числа в діапазоні від 2 до n.*/
+
+let n = +prompt(`Enter a number`, 0);
+mark:
+for (let tempvalue = 2; tempvalue <= n; tempvalue++) {
+    for (let i = 2; i < tempvalue; i++) {
+        if (tempvalue % i == 0)
+        continue mark;    
+    }
+    console.log(`${tempvalue} is a simple number`)
+}
